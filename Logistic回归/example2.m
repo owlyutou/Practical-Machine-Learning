fbdbd86f2a3c95afc3bd6dea4e7dd6b7bbb64f63
Narcissus <2456217567@whut.edu.cn>
2017-11-18 23:00:52 +0800

    为Logistics回归算法补充了疝气病预测案例

--- Logistic回归/example2.m
+++ Logistic回归/example2.m
@@ -1,2 +1,21 @@
 clc, clear
 %% 从疝气病症预测病马的死亡率
+load('horseColicTraining.txt');
+TrainSet = horseColicTraining(:, 1:size(horseColicTraining, 2)-1);
+TrainLabel = horseColicTraining(:, size(horseColicTraining, 2));
+
+load('horseColicTest.txt');
+TestSet = horseColicTest(:, 1:size(horseColicTest, 2)-1);
+TestLabel = horseColicTest(:, size(horseColicTest, 2));
+[numTestVec, ~] = size(TestSet);
+
+trainWeights = stocGradAscent1(TrainSet, TrainLabel, 500);
+
+errCount = 0;
+for i = 1:numTestVec
+    if classifyVector(TestSet(i, :), trainWeights) ~= TestLabel(i)
+        errCount = errCount + 1;
+    end
+end
+errRate = errCount / numTestVec;
+disp(['误分率为：', num2str(errRate)]);
